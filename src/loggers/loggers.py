1f81daa2c4d3b31168ff4b664608326fa9727940
Norceis <cubix77@gmail.com>
2023-10-04 18:44:34 +0200

    added custom format to custom logger

--- src/loggers/loggers.py
+++ src/loggers/loggers.py
@@ -1,15 +1,19 @@
 import logging
 
 
-def get_custom_logger(name, log_file):
-    formatter = logging.Formatter(
-        fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    handler = logging.FileHandler(log_file)
+def get_custom_logger(name: str, log_path: str, format: str):
+    if format:
+        formatter = logging.Formatter(fmt=format)
+    else:
+        formatter = logging.Formatter(
+            fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+    handler = logging.FileHandler(log_path, encoding='utf-8')
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+    logger.handlers = []
     logger.addHandler(handler)
 
     return logger
